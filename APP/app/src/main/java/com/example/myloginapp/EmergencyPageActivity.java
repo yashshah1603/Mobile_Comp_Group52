0163f9600d46b1f7c93260f663a9fb33970cc118
bhargavreddy <brvangal@asu.edu>
2023-12-07 18:58:43 -0700

    Added Emergency Contact Logic

--- APP/app/src/main/java/com/example/myloginapp/EmergencyPageActivity.java
+++ APP/app/src/main/java/com/example/myloginapp/EmergencyPageActivity.java
@@ -1,6 +1,9 @@
 package com.example.myloginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EmergencyPageActivity extends AppCompatActivity {
@@ -9,12 +12,24 @@ public class EmergencyPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_page);
-        // Add your emergency page content and functionality here
+
+        // Find the "End Call" button by its ID
+        ImageButton btnEndCall = findViewById(R.id.btnEndCall);
+
+        // Set an OnClickListener for the "End Call" button
+        btnEndCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click event
+                goToMeasureHeartAndRespRateActivity();
+            }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish(); // Close the EmergencyPageActivity and go back to ExerciseActivity
+    // Method to navigate to the MeasureHeartAndRespRateActivity
+    private void goToMeasureHeartAndRespRateActivity() {
+        Intent intent = new Intent(this, RedirectPage.class);
+        startActivity(intent);
+        finish(); // Close the EmergencyPageActivity
     }
 }
